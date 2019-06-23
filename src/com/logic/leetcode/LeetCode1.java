5b0c080a6ba6c69524a0de97097e90f33efddf49
jiming.luo <jiming.luo@qunar.com>
2019-06-23 21:26:33 +0800

    LeetCode166

--- src/com/logic/leetcode/LeetCode1.java
+++ src/com/logic/leetcode/LeetCode1.java
@@ -7,20 +7,6 @@ import java.util.Map;
 /**
  * LeetCode1
  *
- * @author logic
- * @date 2019/6/23 6:58 PM
- * @since 1.0
- */
-public class LeetCode1 {
-    public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-        System.out.println(Arrays.toString(new Solution1().twoSum(nums, target)));
-        System.out.println(Arrays.toString(new Solution2().twoSum(nums, target)));
-    }
-}
-
-/**
  * 1. Two Sum
  * <p>
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -33,53 +19,64 @@ public class LeetCode1 {
  * <p>
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
+ *
+ * @author logic
+ * @date 2019/6/23 6:58 PM
+ * @since 1.0
  */
+public class LeetCode1 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new Solution1().twoSum(nums, target)));
+        System.out.println(Arrays.toString(new Solution2().twoSum(nums, target)));
+    }
 
-/**
- * 暴力破解方法
- */
-class Solution1 {
-    public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length <= 0) {
-            return null;
-        }
+    /**
+     * 暴力破解方法
+     */
+    private static class Solution1 {
+        public int[] twoSum(int[] nums, int target) {
+            if (nums == null || nums.length <= 0) {
+                return null;
+            }
 
-        int[] result = new int[2];
+            int[] result = new int[2];
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
                 }
             }
-        }
 
-        return null;
-    }
-}
-
-/**
- * 优化后的破解方法
- * 使用Map存储数据
- */
-class Solution2 {
-    public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length <= 0) {
             return null;
         }
+    }
 
-        Map<Integer, Integer> numMap = new HashMap<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            int temp = target - nums[i];
-            if (numMap.containsKey(temp)) {
-                return new int[]{numMap.get(temp), i};
-            } else {
-                numMap.put(nums[i], i);
+    /**
+     * 优化后的破解方法
+     * 使用Map存储数据
+     */
+    private static class Solution2 {
+        public int[] twoSum(int[] nums, int target) {
+            if (nums == null || nums.length <= 0) {
+                return null;
             }
+
+            Map<Integer, Integer> numMap = new HashMap<>(nums.length);
+            for (int i = 0; i < nums.length; i++) {
+                int temp = target - nums[i];
+                if (numMap.containsKey(temp)) {
+                    return new int[]{numMap.get(temp), i};
+                } else {
+                    numMap.put(nums[i], i);
+                }
+            }
+            return new int[]{0, 0};
         }
-        return new int[]{0, 0};
     }
 }
-
