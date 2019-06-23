73ef581fdec943018d72c7fed8615efd066d6599
jiming.luo <jiming.luo@qunar.com>
2019-06-23 20:45:50 +0800

    添加LeetCode1解法

--- src/com/logic/leetcode/LeetCode1.java
+++ src/com/logic/leetcode/LeetCode1.java
@@ -1,5 +1,9 @@
 package com.logic.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * LeetCode1
  *
@@ -8,4 +12,74 @@ package com.logic.leetcode;
  * @since 1.0
  */
 public class LeetCode1 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new Solution1().twoSum(nums, target)));
+        System.out.println(Arrays.toString(new Solution2().twoSum(nums, target)));
+    }
 }
+
+/**
+ * 1. Two Sum
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
+/**
+ * 暴力破解方法
+ */
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 0) {
+            return null;
+        }
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
+/**
+ * 优化后的破解方法
+ * 使用Map存储数据
+ */
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 0) {
+            return null;
+        }
+
+        Map<Integer, Integer> numMap = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (numMap.containsKey(temp)) {
+                return new int[]{numMap.get(temp), i};
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
+
